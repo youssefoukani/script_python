ac90e63828d327f4adcc729c949911d0d89b46d4
youssefoukani <youssefoukani@gmail.com>
2026-02-02 00:18:56 +0100

    Update wait_until_after_midnight function condition
    Change condition to wait until after midnight to check for 23:00 instead of 00:00.

--- script2.py
+++ script2.py
@@ -38,7 +38,7 @@ wait = WebDriverWait(driver, 20)
 def wait_until_after_midnight():
     while True:
         now = datetime.utcnow()
-        if now.hour == 0 and now.minute == 0 and now.second >= 1:
+        if now.hour == 23 and now.minute == 0 and now.second >= 1:
             break
         time.sleep(1)
         
